884ead0ca17ad811ae0f3cda5f86d68d0a289443
wqdsdsf123 <chlxodud04@naver.com>
2024-04-19 00:55:54 +0900

    ...

--- src/pet/dto/create-pet.dto.ts
+++ src/pet/dto/create-pet.dto.ts
@@ -1,13 +1,36 @@
-export class CreatePetDto {
+import { IsString, IsNumber, IsBoolean } from "class-validator";
 
+export class CreatePetDto {
     /**
      * 이름
-     * 생일
-     * 나이
      * 성별
-     * 중성화
+     * 중성화 
      * 분류
-     * 집 등록번호 (없으면 -1)
+     * 칩 등록번호 (없으면 -1)
+     * 생일
+     *  나이
      */
+    @IsString()
+    dogNm : string
+
+    @IsString()
+    sexNm : string
+
+    @IsString()
+    neuterYn : string
+
+    @IsString()
+    kindNm : string
+
+    @IsNumber()
+    rfidCd : number
+
+
+
+
+
+
+
+
 
 }
